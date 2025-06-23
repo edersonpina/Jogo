ed34e9019d4fb2eee5a592bb1c1e1c7fc617830e
edersonpina <ederson.pina.minimal@gmail.com>
2025-06-23 20:45:29 -0300

    Add files via upload

---
+++ lifecycle/FullLifecycleObserver.java
@@ -0,0 +1,5 @@
+package android.arch.lifecycle;
+
+/* loaded from: /storage/emulated/0/Android/data/com.apktools.app.decompile/files/decompile_out/net.ralexander.reforged/classes.dex */
+interface FullLifecycleObserver extends b {
+}
---
+++ lifecycle/c.java
@@ -0,0 +1,7 @@
+package android.arch.lifecycle;
+
+/* compiled from: LifecycleOwner.java */
+/* loaded from: /storage/emulated/0/Android/data/com.apktools.app.decompile/files/decompile_out/net.ralexander.reforged/classes.dex */
+public interface c {
+    a getLifecycle();
+}
