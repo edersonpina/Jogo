a497439072b17b3264df7a79b39c3e213a2d39ae
edersonpina <ederson.pina.minimal@gmail.com>
2025-06-23 22:41:38 -0300

    Add files via upload

---
+++ jogo/source/android/arch/lifecycle/e.java
@@ -0,0 +1,8 @@
+package android.arch.lifecycle;
+
+/* compiled from: LifecycleRegistryOwner.java */
+@Deprecated
+/* loaded from: /storage/emulated/0/Android/data/com.apktools.app.decompile/files/decompile_out/net.ralexander.reforged/classes.dex */
+public interface e extends c {
+    d getLifecycle();
+}
