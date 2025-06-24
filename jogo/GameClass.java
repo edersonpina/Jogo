079c294118b2409ec893873ee75b203f47af240f
edersonpina <ederson.pina.minimal@gmail.com>
2025-06-24 17:25:37 -0300

    Delete jogo/GameClass.java

--- jogo/GameClass.java
+++
@@ -1,7 +0,0 @@
-package net.fdgames.Rules;
-
-/* loaded from: /storage/emulated/0/Android/data/com.apktools.app.decompile/files/decompile_out/net.ralexander.reforged/classes.dex */
-public class GameClass {
-    public int HpPerLevel;
-    public String name;
-}
