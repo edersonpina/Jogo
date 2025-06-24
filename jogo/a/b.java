56dbba3fd031bfd358d4a6f1c9c14afb866fbc31
edersonpina <ederson.pina.minimal@gmail.com>
2025-06-23 21:59:20 -0300

    Add files via upload

---
+++ jogo/a/b.java
@@ -0,0 +1,24 @@
+package a.a;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import net.fdgames.ek.ExiledKingdoms;
+
+0/Android/data/com.
+class b extends InputListener {
+    final /* synthetic */ e a;
+
+    b(e eVar) {
+        this.a = eVar;
+    }
+
+    public boolean touchDown(InputEvent inputEvent, float f, float f2, int i, int i2) {
+        if (ExiledKingdoms.r) {
+            ExiledKingdoms.p.a("ek_donation_con_0");
+        } else {
+            ExiledKingdoms.p.a("ek_donation_con_1");
+        }
+        this.a.a();
+        return true;
+    }
+}
