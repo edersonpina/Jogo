0af27b4c2961fa2b98a70074190b0b544f1ec90b
edersonpina <ederson.pina.minimal@gmail.com>
2025-06-25 15:48:02 -0300

    Add files via upload

---
+++ Jogo/sources/android/arch/lifecycle/FullLifecycleObserver.java
@@ -0,0 +1,5 @@
+package android.arch.lifecycle;
+
+/* loaded from: /storage/emulated/0/Android/data/com.apktools.app.decompile/files/decompile_out/net.newhorizons.now/classes.dex */
+interface FullLifecycleObserver extends b {
+}
